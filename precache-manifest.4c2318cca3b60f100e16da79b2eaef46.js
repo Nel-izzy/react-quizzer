7877488089db2f8957c52d7649451723415992b9
nelson <blackcodedigitals@gmail.com>
2020-07-09 15:37:32 +0100

    Updates

---
+++ precache-manifest.4c2318cca3b60f100e16da79b2eaef46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a6a2d25c9f84a3757e2c719ef7431b",
+    "url": "/react-quizzer/index.html"
+  },
+  {
+    "revision": "f76188a7e894e64f031d",
+    "url": "/react-quizzer/static/css/main.ab9c66f7.chunk.css"
+  },
+  {
+    "revision": "e825d1e118c0518bec69",
+    "url": "/react-quizzer/static/js/2.29bde5aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-quizzer/static/js/2.29bde5aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f76188a7e894e64f031d",
+    "url": "/react-quizzer/static/js/main.7fcddf14.chunk.js"
+  },
+  {
+    "revision": "de744fca727faa32f27b",
+    "url": "/react-quizzer/static/js/runtime-main.23121c5f.js"
+  }
+]);
